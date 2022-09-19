a5e585dc6dda1241923b121fbafd89439c2de969
Arunesh Naha <aruneshnaha18@gmail.com>
2022-09-19 10:01:02 +0530

    implementing npm cors for backend

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
+const cors = require('cors')
 const cookieParser = require('cookie-parser')
 
 app.use(express.json());
 app.use(cookieParser())
+app.use(cors())
 
 //Route imports
 const product = require('./routes/productRoute');
