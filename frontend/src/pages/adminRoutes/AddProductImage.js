510f5bf22bcb776b6bfaa79e2b0dfb718c1cd72b
Arunesh Naha <aruneshnaha18@gmail.com>
2022-10-08 09:40:37 +0530

    add Product with image started working

--- frontend/src/pages/adminRoutes/AddProductImage.js
+++ frontend/src/pages/adminRoutes/AddProductImage.js
@@ -4,49 +4,10 @@ import Button from '@mui/material/Button';
 
 export default function AddProductImage(props) {
   const { productId } = useParams();
-  const navigate = useNavigate()
-  const [showUploadButton, setShowUploadButton] = useState(0)
+  const navigate = useNavigate();
 
   const formdata = new FormData();
   const host = 'http://localhost:4000';
-
-  const handleChange = (e) => {
-    const value = e.target.files[0];
-    console.log(value);
-    formdata.append('id', productId);
-
-    formdata.append('images', value);
-    setShowUploadButton(1)
-    for (var key of formdata.entries()) {
-      console.log(key[0] + ': ' + key[1]);
-    }
-  };
-
-  const uploadImage = async () => {
-    const token = localStorage.getItem('token');
-    if (token !== null) {
-      try {
-        const response = await fetch(`${host}/api/v1/upload`, {
-          method: 'POST',
-          body: formdata,
-        });
-
-        const res = await response.json();
-        if (response.status === 200) {
-          console.log(`Message from upload product image: ${res.message}`);
-          props.showAlert('Product image uploaded successfully!', 'success');
-          navigate('/adminDashboard')
-        } else {
-          props.showAlert('Failed to create product', 'danger');
-        }
-        console.log(res);
-      } catch (error) {
-        console.log(error);
-      }
-    } else {
-      props.showAlert('Please login as admin first', 'danger');
-    }
-  };
 
   return (
     <div>
@@ -58,14 +19,92 @@ export default function AddProductImage(props) {
             name="images"
             placeholder="choose a file"
             type="file"
-            onChange={handleChange}
+            onChange={async (e) => {
+              const value = e.target.files[0];
+
+              console.log(value);
+
+              formdata.append('id', productId);
+              formdata.append('images', value);
+
+              for (var key of formdata.entries()) {
+                console.log(key[0] + ': ' + key[1]);
+              }
+
+              const token = localStorage.getItem('token');
+              if (token !== null) {
+                try {
+                  const response = await fetch(`${host}/api/v1/upload`, {
+                    method: 'POST',
+                    body: formdata,
+                  });
+
+                  const res = response;
+                  if (response.status === 200) {
+                    console.log(
+                      `Message from upload product image: ${res.message}`
+                    );
+                    props.showAlert(
+                      'Product image uploaded successfully!',
+                      'success'
+                    );
+                    navigate('/adminDashboard');
+                  } else {
+                    console.log(
+                      `Message from upload product image: ${res.message}`
+                    );
+                    props.showAlert('Failed to create product', 'danger');
+                  }
+                  console.log(res);
+                } catch (error) {
+                  console.log(error);
+                }
+              } else {
+                props.showAlert('Please login as admin first', 'danger');
+              }
+            }}
           />
         </Button>
       </center>
       <center>
-      {showUploadButton ? <button className="btn btn-primary m-3" onClick={uploadImage}>
-        Upload this image
-      </button> : ''}
+        <button
+          className="btn btn-primary m-3"
+          // onClick={async () => {
+          //   const token = localStorage.getItem('token');
+          //   if (token !== null) {
+          //     try {
+          //       const response = await fetch(`${host}/api/v1/upload`, {
+          //         method: 'POST',
+          //         body: formdata,
+          //       });
+
+          //       const res = response;
+          //       if (response.status === 200) {
+          //         console.log(
+          //           `Message from upload product image: ${res.message}`
+          //         );
+          //         props.showAlert(
+          //           'Product image uploaded successfully!',
+          //           'success'
+          //         );
+          //         navigate('/adminDashboard');
+          //       } else {
+          //         console.log(
+          //           `Message from upload product image: ${res.message}`
+          //         );
+          //         props.showAlert('Failed to create product', 'danger');
+          //       }
+          //       console.log(res);
+          //     } catch (error) {
+          //       console.log(error);
+          //     }
+          //   } else {
+          //     props.showAlert('Please login as admin first', 'danger');
+          //   }
+          // }}
+        >
+          Upload this image
+        </button>
       </center>
     </div>
   );
